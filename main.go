4d2b781c37a0f142f0ba5decd333dab8fe2a1987
Galih Muhammad <galih0muhammad@gmail.com>
2017-12-28 17:23:06 +0700

    Add looping each row

--- main.go
+++ main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+    "log"
+    "fmt"
+    "github.com/PuerkitoBio/goquery"
     "github.com/gocolly/colly"
 )
 
-const tokenListURL string = "https://tokenmarket.net/blockchain/ethereum/assets/"
+const tokenListURL string = "https://erc20-tokens.eidoo.io"
+
 
 func main() {
     // Instantiate default collector
     c := colly.NewCollector()
 
+    // Before making a request print "Visiting ..."
+    c.OnRequest(func(r *colly.Request) {
+        log.Println("visiting", r.URL.String())
+    })
+
+    // Extract token
+    c.OnHTML("#tokensTable tbody", func(e *colly.HTMLElement) {
+        e.DOM.Find("tr#coinRow").Each(func(_ int, sel *goquery.Selection) {
+            name := sel.Find(".coin h4").Text()
+            price := sel.Find(".price h4").Text()
+
+            fmt.Printf("%v %v \n", name, price)
+        })        
+    })
 
     c.Visit(tokenListURL)
 }
